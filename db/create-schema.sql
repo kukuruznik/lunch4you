d5ac71ea400383994ea60192a94ff7f7e1d1d81e
Michal Galbavy <mgalbavy@gmail.com>
2012-04-17 21:48:12 +0200

    updated DB schema

--- db/create-schema.sql
+++ db/create-schema.sql
@@ -1,10 +1,69 @@
--- Table "meal" DDL
-
-CREATE TABLE `meal` (
+-- Table "article" DDL
+CREATE TABLE `article` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `daily_limit` int(11) DEFAULT NULL,
   `description` varchar(1000) DEFAULT NULL,
   `name` varchar(255) NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
   `version` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- Table "customer" DDL
+CREATE TABLE `customer` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `token` varchar(50) NOT NULL,
+  `credit` int(11) NOT NULL DEFAULT '0',
+  `version` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Table "isprepared" DDL
+CREATE TABLE `isprepared` (
+  `preparationid` bigint(20) NOT NULL,
+  `orderitemid` bigint(20) NOT NULL,
+  KEY `isprepared_preparation` (`preparationid`),
+  KEY `isprepared_orderitem` (`orderitemid`),
+  CONSTRAINT `isprepared_orderitem` FOREIGN KEY (`orderitemid`) REFERENCES `orderitem` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `isprepared_preparation` FOREIGN KEY (`preparationid`) REFERENCES `preparation` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Table "order" DDL
+CREATE TABLE `order` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `ownerid` bigint(20) NOT NULL,
+  `total` int(11) NOT NULL DEFAULT '0',
+  `version` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `orders_owner` (`ownerid`),
+  CONSTRAINT `orders_owner` FOREIGN KEY (`ownerid`) REFERENCES `customer` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Table "orderitem" DDL
+CREATE TABLE `orderitem` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `count` int(11) NOT NULL,
+  `unitprice` int(11) NOT NULL,
+  `totalprice` int(11) NOT NULL,
+  `articleid` bigint(20) NOT NULL,
+  `version` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `orderitems_article` (`articleid`),
+  CONSTRAINT `orderitems_article` FOREIGN KEY (`articleid`) REFERENCES `article` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Table "preparation" DDL
+CREATE TABLE `preparation` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `status` varchar(50) NOT NULL,
+  `articleid` bigint(20) NOT NULL,
+  `count` int(11) NOT NULL DEFAULT '0',
+  `version` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `preparations_article` (`articleid`),
+  CONSTRAINT `preparations_article` FOREIGN KEY (`articleid`) REFERENCES `article` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
