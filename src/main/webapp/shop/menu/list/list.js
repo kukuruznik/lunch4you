8b5bed6d5c2ca5ed75973afb1126772015ba8253
ladi <ladislav.nemeth@gmail.com>
2012-11-13 23:21:07 +0100

    online menu re-worked

--- src/main/webapp/shop/menu/list/list.js
+++ src/main/webapp/shop/menu/list/list.js
@@ -1,4 +1,4 @@
-steal( 'jquery/controller', 'jquery/view/ejs', 'jquery/controller/view' ).then( './views/list.ejs', /*'./views/article.ejs',*/ function( $ ) {
+steal( 'jquery/controller', 'jquery/view/ejs', 'jquery/controller/view' ).then( './views/list.ejs', function( $ ) {
 
 	/**
 	 * @class Shop.Menu.List
