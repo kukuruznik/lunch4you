7b6aff86c1936721381417980d5884657ed39d6d
ladi <ladislav.nemeth@gmail.com>
2012-12-12 22:50:37 +0100

    .

--- src/main/webapp/common/register_customer/register_customer.js
+++ src/main/webapp/common/register_customer/register_customer.js
@@ -20,7 +20,7 @@ steal( 'jquery/controller', 'jquery/view/ejs', 'jquery/controller/view', 'jquery
 			this._render();
 		},
 
-		"input[type=submit] click": function( el, evt ) {
+		"#submitButton click": function( el, evt ) {
 			var formParams = this.element.find( "form" ).formParams();
 			this.element.trigger( "updateProfile", formParams );
 			evt.preventDefault();
