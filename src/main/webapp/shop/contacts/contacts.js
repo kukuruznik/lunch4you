20a2c121a208b85250c548cf75fdedf12bcb33fb
ladi <ladislav.nemeth@gmail.com>
2013-01-27 13:20:00 +0100

    Contacts page enhanced

--- src/main/webapp/shop/contacts/contacts.js
+++ src/main/webapp/shop/contacts/contacts.js
@@ -1,4 +1,8 @@
-steal( 'jquery/controller', 'jquery/view/ejs', 'jquery/controller/view' ).then( function( $ ) {
+steal( 'jquery/controller', 'jquery/view/ejs', 'jquery/controller/view' ).then( 
+
+	"./views/contacts.ejs",
+
+	function( $ ) {
 
 	/**
 	 * @class Shop.Ordering
