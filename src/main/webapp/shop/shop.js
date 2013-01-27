ee4080269199bc1d393084f25c753e01bf378c69
ladi <ladislav.nemeth@gmail.com>
2013-01-27 13:30:42 +0100

    most of the app includes listed already in this first file.

--- src/main/webapp/shop/shop.js
+++ src/main/webapp/shop/shop.js
@@ -1,6 +1,21 @@
 steal(
-	'shop/shop.css', 			// application CSS file
-	'shop/models/models.js'	// steals all your models
+	"jquery/controller" 
+	,"jquery/view/ejs" 
+	,"jquery/controller/view" 
+	,"jquery/event/bbq"
+	,"jquery/dom/cookie"
+	,"shop/utils/utils.js"
+	,"common/register_customer/register_customer.js"
+	,"shop/menu/list/list.js"
+	,"shop/order/new/newOrder.js"
+	,"shop/home/home.js"
+	,"shop/contacts/contacts.js"
+	,"shop/ordering/ordering.js"
+	,"shop/referral/new/newReferral.js"
+	,"shop/profile/edit/editProfile.js"
+	,"shop/shop.css" 			// application CSS file
+	,"common/form.css" 			// forms CSS file
+	,"shop/models/models.js"		// steals all your models
 ).then(
 	"shop/main",				// main controller...
 
