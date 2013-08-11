f662c926850cb4d3914c4bead19df80e09b2d5ef
ladi <ladislav.nemeth@gmail.com>
2013-08-11 20:21:03 +0200

    Labels Printing changed to 10 labels per page

--- src/main/webapp/backoffice/label/print/print.js
+++ src/main/webapp/backoffice/label/print/print.js
@@ -13,7 +13,7 @@ steal( "jquery/controller", "jquery/view/ejs", "jquery/controller/view", "backof
 	/** @Prototype */
 	{
 		init: function() {
-			this.rows = 6;
+			this.rows = 5;
 			this.cols = 2; // would be fine to externalize
 
 			this._refresh();
